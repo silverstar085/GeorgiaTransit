22de86122b22cd95be5be77e3e2767596d70dc39
Neil Shah <shahneil1909@gmail.com>
2016-07-24 10:47:41 -0400

    Update script.js

--- source/js/script.js
+++ source/js/script.js
@@ -10,7 +10,7 @@ document.getElementById("time").innerHTML = datetime;
 //____________________________
 
 var xmlhttp = new XMLHttpRequest();
-var url = "https://jsonp.afeld.me/?url=http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetAllBus";
+var url = "https://crossorigin.me/http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetAllBus";
 if('withCredentials' in xmlhttp)
 {    
 xmlhttp.open("GET", url, true);  
